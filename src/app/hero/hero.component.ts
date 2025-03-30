9c5f0c9b58d1e5050bf982957a53d8ebd9f1ba16
yassir <yassiraitlahmadi@gmail.com>
2025-03-30 15:46:52 +0000

    modify the home page

--- src/app/hero/hero.component.ts
+++ src/app/hero/hero.component.ts
@@ -1,9 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { Component, ChangeDetectorRef } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-hero',
-  imports: [CommonModule],
+  imports: [CommonModule,RouterLink],
   templateUrl: './hero.component.html',
   styleUrls: ['./hero.component.css']
 })
