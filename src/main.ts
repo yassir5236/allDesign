58de11da272935c1fc41b10113064b7c3f5db6a0
yassir <yassiraitlahmadi@gmail.com>
2025-07-17 19:58:57 +0100

    last one

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,29 @@
-import { bootstrapApplication } from '@angular/platform-browser';
+/* import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 
-bootstrapApplication(AppComponent, appConfig)
+bootstrapApplication(AppComponent,{
+  providers: [provideRouter(routes, withHashRouting())]
+}, appConfig);
   .catch((err) => console.error(err));
+ */
+
+
+
+  import { bootstrapApplication } from '@angular/platform-browser';
+  import { AppComponent } from './app/app.component';
+  import { appConfig } from './app/app.config';
+  import { provideRouter } from '@angular/router';
+  import { routes } from './app/app.routes';
+  
+  import { LocationStrategy, HashLocationStrategy } from '@angular/common';
+  
+  bootstrapApplication(AppComponent, {
+    ...appConfig,
+    providers: [
+      ...(appConfig.providers || []),
+      provideRouter(routes),
+      { provide: LocationStrategy, useClass: HashLocationStrategy }
+    ]
+  }).catch((err) => console.error(err));
+  
